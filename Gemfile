a3362ea9bf82662f0689ef39390d984efcd24342
Jing Li <jingli@ebay.com>
2015-07-28 11:49:46 +0200

    Set up test coverage from Code Climate

--- Gemfile
+++ Gemfile
@@ -5,4 +5,5 @@ gem 'nokogiri'
 group :test do
   gem 'rake'
   gem 'minitest'
+  gem 'codeclimate-test-reporter'
 end
--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,6 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 $:.unshift(File.expand_path(File.join(File.dirname(__FILE__), *%w[.. lib junit_report_generator])))
 require 'junit_report_generator'
 require 'minitest/autorun'

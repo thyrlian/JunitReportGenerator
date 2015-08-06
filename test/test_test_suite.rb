a64695f42389d76c7aac3f5b26e5797ca98515cc
Jing Li <jingli@ebay.com>
2015-08-06 02:30:05 +0200

    Fix mock in test

--- test/test_test_suite.rb
+++ test/test_test_suite.rb
@@ -6,9 +6,6 @@ class TestTestSuite < MiniTest::Unit::TestCase
     @test_case_1 = MiniTest::Mock.new
     @test_case_2 = MiniTest::Mock.new
     @test_case_3 = MiniTest::Mock.new
-    @test_case_1.expect(:hash, 1000000000000000001)
-    @test_case_2.expect(:hash, 1000000000000000002)
-    @test_case_3.expect(:hash, 1000000000000000003)
   end
   
   def test_create
@@ -21,11 +18,20 @@ class TestTestSuite < MiniTest::Unit::TestCase
   end
   
   def test_add_by_splatting_arguments
+    2.times do
+      @test_case_1.expect(:hash, 1000000000000000001)
+      @test_case_2.expect(:hash, 1000000000000000002)
+      @test_case_3.expect(:hash, 1000000000000000003)
+    end
     @test_suite.add(@test_case_1, @test_case_2, @test_case_3)
     assert_equal(3, @test_suite.instance_variable_get(:@tests))
   end
   
   def test_add_by_chaining
+    2.times do
+      @test_case_1.expect(:hash, 1000000000000000001)
+      @test_case_2.expect(:hash, 1000000000000000002)
+    end
     @test_suite.add(@test_case_1).add(@test_case_2)
     assert_equal(2, @test_suite.instance_variable_get(:@tests))
   end

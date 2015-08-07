af5a53e295814ecfa467820919c4cf17642eca42
Jing Li <jingli@ebay.com>
2015-08-07 21:11:50 +0200

    refactor generator test

--- test/test_generator.rb
+++ test/test_generator.rb
@@ -1,7 +1,7 @@
 require_relative 'helper'
 
 class TestGenerator < Minitest::Test
-  def setup
+  def test_produce
     test_suites = TestSuites.create
     test_suite = TestSuite.create('UI-Tests')
     test_suite.add(TestCase.create('testHome').classname('HomeActivityTest').time('2.081'))
@@ -9,13 +9,11 @@ class TestGenerator < Minitest::Test
       .add(TestCase.create('testLoginWithWrongCredentials').classname('LoginActivityTest').time('6.691'))
     test_suites.add(test_suite)
     xml = Generator.produce(test_suites)
-    @doc = Nokogiri::Slop(xml)
-  end
-  
-  def test_produce
-    assert_equal(3, @doc.testsuites.testsuite.testcase.size)
-    assert_equal('testLoginWithWrongCredentials', @doc.testsuites.testsuite.testcase.last['name'])
-    assert_equal('LoginActivityTest', @doc.testsuites.testsuite.testcase.last['classname'])
-    assert_equal('6.691', @doc.testsuites.testsuite.testcase.last['time'])
+    doc = Nokogiri::Slop(xml)
+    
+    assert_equal(3, doc.testsuites.testsuite.testcase.size)
+    assert_equal('testLoginWithWrongCredentials', doc.testsuites.testsuite.testcase.last['name'])
+    assert_equal('LoginActivityTest', doc.testsuites.testsuite.testcase.last['classname'])
+    assert_equal('6.691', doc.testsuites.testsuite.testcase.last['time'])
   end
 end

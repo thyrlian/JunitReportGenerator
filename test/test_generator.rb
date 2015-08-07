782cefdb5c58473604409787d7ca6243ac1107c0
Jing Li <jingli@ebay.com>
2015-08-07 21:56:25 +0200

    Add test for Generator#build_tags

--- test/test_generator.rb
+++ test/test_generator.rb
@@ -16,4 +16,28 @@ class TestGenerator < Minitest::Test
     assert_equal('LoginActivityTest', doc.testsuites.testsuite.testcase.last['classname'])
     assert_equal('6.691', doc.testsuites.testsuite.testcase.last['time'])
   end
+  
+  def test_build_tags_without_recursion
+    xml = Nokogiri::XML::Builder.new do |xml|
+      Generator.send(:build_tags, xml, TestSuites.create)
+    end.to_xml
+    
+    assert(Nokogiri::XML(xml).xpath('//testsuites').children.empty?)
+  end
+  
+  def test_build_tags_with_sub_elements
+    test_suites = TestSuites.create
+    test_suite = TestSuite.create('UI-Tests')
+    test_suite.add(TestCase.create('testHome').classname('HomeActivityTest').time('2.081'))
+      .add(TestCase.create('testLoginWithCorrectCredentials').classname('LoginActivityTest').time('7.472'))
+      .add(TestCase.create('testLoginWithWrongCredentials').classname('LoginActivityTest').time('6.691'))
+    test_suites.add(test_suite)
+    
+    xml = Nokogiri::XML::Builder.new do |xml|
+      Generator.send(:build_tags, xml, test_suites)
+    end.to_xml
+    
+    assert(!Nokogiri::XML(xml).xpath('//testsuites').children.empty?)
+    assert_equal(3, Nokogiri::XML(xml).xpath('//testcase').size)
+  end
 end

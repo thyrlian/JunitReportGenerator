81f55217183fd2e08f57ebc8169c64da2fa08053
Jing Li <jingli@ebay.com>
2015-08-09 12:18:52 +0200

    Fix tests

--- test/test_test_suite.rb
+++ test/test_test_suite.rb
@@ -10,7 +10,8 @@ class TestTestSuite < Minitest::Test
   
   def test_create
     assert_equal('Sonar', @test_suite.instance_variable_get(:@name))
-    assert_equal({:name => 'Sonar', :tests => 0}, @test_suite.attributes)
+    assert_equal('Sonar', @test_suite.attributes[:name])
+    assert_equal(0, @test_suite.attributes[:tests])
     assert_equal([], @test_suite.sub_elements)
   end
   

301f12ec81db630a5c74f1184d4c76432c53b4d1
Jing Li <jingli@ebay.com>
2015-08-12 16:56:10 +0200

    Add the very important initialize_attributes_if_not_yet

--- lib/junit_report_generator/elementary.rb
+++ lib/junit_report_generator/elementary.rb
@@ -12,6 +12,7 @@ module JunitReportGenerator
     end
     
     def update_attributes(*attrs)
+      initialize_attributes_if_not_yet
       attrs.each do |attr|
         attr = attr.to_sym
         instance_variable_get(:@attributes)[attr] = instance_variable_get("@#{attr.id2name}")
@@ -19,12 +20,22 @@ module JunitReportGenerator
     end
     
     def method_missing(method, *args)
+      initialize_attributes_if_not_yet
       instance_variable_get(:@attributes)[method] = args.first
       self.class.send(:define_method, method) do |value|
+        initialize_attributes_if_not_yet
         instance_variable_get(:@attributes)[method] = value
         self
       end
       self
     end
+    
+    def initialize_attributes_if_not_yet
+      unless instance_variable_get(:@attributes)
+        instance_variable_set(:@attributes, {})
+      end
+    end
+    
+    private :initialize_attributes_if_not_yet
   end
 end

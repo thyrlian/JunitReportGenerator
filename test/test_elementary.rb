81017a282e8b7bca498f4adf55b3ca619ec09c0b
Jing Li <jingli@ebay.com>
2015-08-13 06:35:34 +0200

    Refactor tests

--- test/test_elementary.rb
+++ test/test_elementary.rb
@@ -2,7 +2,7 @@ require_relative 'helper'
 
 class TestElementary < Minitest::Test
   def setup
-    @klass = Class.new do
+    @klass_with_dflt_attrs = Class.new do
       include Elementary
       
       def initialize(name, value)
@@ -17,27 +17,32 @@ class TestElementary < Minitest::Test
       end
     end
     
-    @object = @klass.new('foobar', 88)
+    @klass_without_anything = Class.new do
+      include Elementary
+    end
+    
+    @obj_with_dflt_attrs = @klass_with_dflt_attrs.new('foobar', 88)
+    @obj_without_dflt_attrs = @klass_without_anything.new
   end
   
   def test_assemble_attributes
-    assert_equal({:name => 'foobar', :value => 88}, @object.attributes)
+    assert_equal({:name => 'foobar', :value => 88}, @obj_with_dflt_attrs.attributes)
   end
   
   def test_update_attributes
-    @object.change(1024)
-    assert_equal({:name => 'foobar', :value => 1024}, @object.attributes)
+    @obj_with_dflt_attrs.change(1024)
+    assert_equal({:name => 'foobar', :value => 1024}, @obj_with_dflt_attrs.attributes)
   end
   
   def test_method_missing_should_add_optional_attribute
-    @object.status('Unknown')
-    assert_equal({:name => 'foobar', :value => 88, :status => 'Unknown'}, @object.attributes)
+    @obj_with_dflt_attrs.status('Unknown')
+    assert_equal({:name => 'foobar', :value => 88, :status => 'Unknown'}, @obj_with_dflt_attrs.attributes)
   end
   
   def test_method_missing_should_cache_optional_attribute_to_method
-    assert(!@klass.instance_methods.include?(:status))
-    @object.status('Unknown')
-    assert(@klass.instance_methods.include?(:status))
-    assert_equal({:name => 'foobar', :value => 88, :status => 'Unknown'}, @object.attributes)
+    assert(!@klass_with_dflt_attrs.instance_methods.include?(:status))
+    @obj_with_dflt_attrs.status('Unknown')
+    assert(@klass_with_dflt_attrs.instance_methods.include?(:status))
+    assert_equal({:name => 'foobar', :value => 88, :status => 'Unknown'}, @obj_with_dflt_attrs.attributes)
   end
 end

07d00cef4af0b8524a1cd0dfc0d9d5622f67e914
Jing Li <jingli@ebay.com>
2015-08-16 11:03:03 +0200

    Disable CodeClimate to try Coveralls

--- test/helper.rb
+++ test/helper.rb
@@ -1,8 +1,8 @@
 require 'coveralls'
 Coveralls.wear!
 
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+# require 'codeclimate-test-reporter'
+# CodeClimate::TestReporter.start
 
 $:.unshift(File.expand_path(File.join(File.dirname(__FILE__), *%w[.. lib junit_report_generator])))
 require 'junit_report_generator'

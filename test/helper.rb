bb2dd096265255145c41ef1746541d093317bd1f
Jing Li <jingli@ebay.com>
2015-08-18 22:15:12 +0200

    Use the correct delimiters for %w-literals

--- test/helper.rb
+++ test/helper.rb
@@ -1,4 +1,4 @@
-$:.unshift(File.expand_path(File.join(File.dirname(__FILE__), *%w[.. lib junit_report_generator])))
+$:.unshift(File.expand_path(File.join(File.dirname(__FILE__), *%w(.. lib junit_report_generator))))
 
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start

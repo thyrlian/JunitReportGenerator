e8e9807fb343360b3e67d5b4f7431c3e5d1ffa30
Jing Li <jingli@ebay.com>
2015-08-19 21:24:59 +0200

    Remove redundant curly braces around hash parameter

--- test/test_test_suites.rb
+++ test/test_test_suites.rb
@@ -22,14 +22,14 @@ class TestTestSuites < Minitest::Test
   end
 
   def test_register_block
-    @test_suite_1.expect(:attributes, { tests: 5, failures: 1, time: 60 })
+    @test_suite_1.expect(:attributes, tests: 5, failures: 1, time: 60)
     @test_suite_2.expect(:attributes, {})
-    @test_suite_3.expect(:attributes, { tests: 0, errors: 0, failures: 0, disabled: 0, time: 0 })
-    @test_suite_4.expect(:attributes, { tests: 10, errors: 2, failures: 0, disabled: 0, time: 100 })
-    @test_suite_5.expect(:attributes, { tests: 5, errors: 0, failures: 0, disabled: 2, time: 50.078 })
-    @test_suite_6.expect(:attributes, { tests: 3, errors: 1, failures: 1, disabled: 1, time: 10.0009 })
-    @test_suite_7.expect(:attributes, { tests: 6, errors: 2, failures: 2, time: 0 })
-    @test_suite_8.expect(:attributes, { tests: 1, errors: 0, failures: 0, disabled: 0, time: 4.6 })
+    @test_suite_3.expect(:attributes, tests: 0, errors: 0, failures: 0, disabled: 0, time: 0)
+    @test_suite_4.expect(:attributes, tests: 10, errors: 2, failures: 0, disabled: 0, time: 100)
+    @test_suite_5.expect(:attributes, tests: 5, errors: 0, failures: 0, disabled: 2, time: 50.078)
+    @test_suite_6.expect(:attributes, tests: 3, errors: 1, failures: 1, disabled: 1, time: 10.0009)
+    @test_suite_7.expect(:attributes, tests: 6, errors: 2, failures: 2, time: 0)
+    @test_suite_8.expect(:attributes, tests: 1, errors: 0, failures: 0, disabled: 0, time: 4.6)
 
     test_suites = TestSuites.create
     test_suites.add(@test_suite_1, @test_suite_2, @test_suite_3, @test_suite_4, @test_suite_5, @test_suite_6, @test_suite_7, @test_suite_8)

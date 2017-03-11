a1e092393cd382aedccc020c64d663c0ff0f955e
Jing Li <thyrlian@gmail.com>
2017-03-11 09:04:46 +0100

    Try to fix the deprecated usage of Code Climate Test Reporter

--- test/helper.rb
+++ test/helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), *%w(.. lib junit_report_generator))))
 
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+require 'simplecov'
+SimpleCov.start
 
 require 'coveralls'
 Coveralls.wear!
